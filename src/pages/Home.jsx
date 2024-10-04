421b8a9d1d1d53340f71e8e091cb7037a64122f9
Rika Kamila <rikakamila1717@gmail.com>
2024-10-04 14:13:43 +0700

    import Katalog n Footer to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Menubar from '../components/Menubar';
 import Hero from '../components/Hero';
+import Katalog from '../components/Katalog';
+import Footer from '../components/Footer';
 
 function Home() {
   return (
@@ -9,6 +11,8 @@ function Home() {
         <Navbar/>
         <Hero/>
         <Menubar/>
+        <Katalog/>
+        <Footer/>
       
     </div>
   );
