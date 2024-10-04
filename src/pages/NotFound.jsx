3c6d62c60b482ad809ccdba14cf178d1bbb2aea4
Rika Kamila <rikakamila1717@gmail.com>
2024-10-04 14:26:54 +0700

    Add pags Not found

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <h1 className="text-3xl text-black">404 Not Found</h1>
+    </div>
+  );
+}
+
+export default NotFound;
