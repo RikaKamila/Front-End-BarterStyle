1e200b18dcbf6045f336a572a30274b294a2409d
Rika Kamila <rikakamila1717@gmail.com>
2024-10-04 14:28:14 +0700

    Add content Contact n styling

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <>
+    <div className="w-screen flex flex-col items-center">
+ 
+        <div className="w-full h-[150px] bg-teal-500 flex justify-center items-center font-bold text-3xl">
+            <h1 className="text-center items-center">CONTACT US</h1>
+        </div>
+        <div className="w-full h-[300px] justify-center text-center flex flex-col">
+          <h2 className="text-2xl p-1">Jalan Mampang Prapatan Jakarta Selatan 12790</h2>
+          <h2 className="text-2xl p-1">08818181818</h2>
+          <h2 className="text-2xl p-1">info.barterstyle.com</h2>
+        </div>
+
+        <div className="w-[50%] p-[50px] text-center justify-center">
+          <h3 className="text-2xl font-bold pb-5">Contact Us for More Information!</h3>
+          <button className="border rounded-full bg-amber-400 hover:bg-amber-300 p-3 font-semibold">Contact Us</button>
+        </div>
+    </div>
+
+    </>
+  );
+}
+
+export default Contact;
