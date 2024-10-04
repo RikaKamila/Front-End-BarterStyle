dd7f345bcf4d53e841d562aa56621ab9da122ba3
Rika Kamila <rikakamila1717@gmail.com>
2024-10-04 16:30:25 +0700

    clear 'class' n switch className in katalog

--- src/components/Katalog.jsx
+++ src/components/Katalog.jsx
@@ -18,15 +18,14 @@ useEffect(() => {
 }, []);
   
   return (
-<div class="container mx-auto px-4 p-5 sm:px-6 lg:px-8">
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">   
-
+<div className="container mx-auto px-4 p-5 sm:px-6 lg:px-8">
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[50px] "> 
     {products.map(prod => (
-      <div class="bg-white rounded shadow-md p-4 hover:bg-gray-100 flex flex-col items-center justify-center" key={prod.id}>
+      <div className="bg-white rounded shadow-md p-4 hover:bg-gray-100 flex items-center justify-center" key={prod.id}>
         <Link to={`/Detail`} state={{ product: prod }}>
-          <img src={prod?.image} alt={prod?.title} class="w-full h-[200px] object-cover rounded-t-lg" />
-          <div class="p-4 text-center">
-            <h3 class="text-xl font-bold">{prod?.title}</h3>
+          <img src={prod?.image} alt={prod?.title} className="w-[200px] h-[200px] object-cover rounded-t-lg " />
+          <div className="p-4 text-center">
+            <h3 className="text-xl font-bold">{prod?.title}</h3>
           </div>
         </Link>
       </div>
